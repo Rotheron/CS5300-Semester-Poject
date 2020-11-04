9e40238d8a4055a7bd9421b1e2f1e192893b67de
ColeDavis99 <coledavis6499@gmail.com>
2020-11-04 13:11:29 -0600

    Coles first push

--- Data_import.py
+++ Data_import.py
@@ -1,42 +1,60 @@
 import pandas as pd
 import mariadb as db
 import re 
-import sys
-# cd into this directory to run the code \\minerfiles.mst.edu\dfs\users\ztbmwf\CS5300\Semester Project
-#push test
+
 
 def book_clean(data):
     # preprocess and clean the data for the "book" column in the data frame (df)
-    book = data['book']
-    #book = pd.DataFrame([['Sacramento', 'California'], ['Miami', 'Florida']], columns=['City', 'State'])
-    book.to_csv('book.csv')
+    s = data['book']
+    print(s)
 
 
 def title_clean(data):
-    pass
+    #Read in raw title column.
+    titles = data['title']
+    
+    #Indexes of valid Titles (0 through 12042)
+    idxToTitle = dict()
 
-def author_clean(data):
-    author = data['author']
-    print(author)
-    pass
-def binding_clean(data):
-    pass
+    #Keep only titles consisting of just these characters
+    validChars = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ")
+    
+    #Does this entire title consist of just validChars?
+    titleIsValid = True
+
+    #Iterate over every title and store the dataframe indeces of titles with only alphabetic characters in it
+    for i in range(len(titles)):                    #Title Loop
+        validTitle = True
+        for q in range(len(titles[i])):             #Character Loop
+            if(titles[i][q] not in validChars):
+                validTitle = False
+                break
+
+        if(validTitle):
+            idxToTitle[i] = str(titles[i])
+
+    titleExport = pd.DataFrame.from_dict(idxToTitle, orient="index")
+
+    #Write output to csv
+    titleExport.to_csv(r'titles.csv')
+
 def db_connection():
     try:
-        conn = db.connect(
+        conn = mariadb.connect(
             user="db_user",
             password="db_user_passwd",
             host="192.0.2.1",
             port=3306,
             database="employees"
     )
-    except db.Error as e:
+    except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
         sys.exit(1)
 
 if __name__ == "__main__":
-    data = pd.read_csv(r'\\minerfiles.mst.edu\dfs\users\ztbmwf\CS5300\Semester Project\inventory.csv', encoding = "ISO-8859-1")   
+    data = pd.read_csv('inventory.csv', encoding = "ISO-8859-1")   
     df = pd.DataFrame(data, columns= ['book','title','author','binding','pubdate','publisher','isbn10','isbn13','condition','dustjacket','signed','edition','price','descr','synopsis','about_auth'])
-    book_clean(df)
-    author_clean(df)
-    pass
+    
+    
+    #book_clean(df)
+    title_clean(df)
