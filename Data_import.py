6c2adde7b70663d1dcf191f01d9e58b4684b186f
Zachary Bingham <lilbing99@gmail.com>
2020-11-04 15:51:46 -0600

    commented out the database connection for the time being

--- Data_import.py
+++ Data_import.py
@@ -5,8 +5,9 @@ import re
 
 def book_clean(data):
     # preprocess and clean the data for the "book" column in the data frame (df)
-    s = data['book']
-    print(s)
+    book = data['book']
+    #book = pd.DataFrame([['Sacramento', 'California'], ['Miami', 'Florida']], columns=['City', 'State'])
+    book.to_csv('Clean_Data/book.csv')
 
 
 def title_clean(data):
@@ -20,7 +21,7 @@ def title_clean(data):
     validChars = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ")
     
     #Does this entire title consist of just validChars?
-    titleIsValid = True
+    validTitle = True
 
     #Iterate over every title and store the dataframe indeces of titles with only alphabetic characters in it
     for i in range(len(titles)):                    #Title Loop
@@ -38,23 +39,23 @@ def title_clean(data):
     #Write output to csv
     titleExport.to_csv(r'titles.csv')
 
-def db_connection():
-    try:
-        conn = mariadb.connect(
-            user="db_user",
-            password="db_user_passwd",
-            host="192.0.2.1",
-            port=3306,
-            database="employees"
-    )
-    except mariadb.Error as e:
-        print(f"Error connecting to MariaDB Platform: {e}")
-        sys.exit(1)
+# def db_connection():
+#     try:
+#         conn = db.connect(
+#             user="db_user",
+#             password="db_user_passwd",
+#             host="192.0.2.1",
+#             port=3306,
+#             database="employees"
+#     )
+#     except db.Error as e:
+#         print(f"Error connecting to MariaDB Platform: {e}")
+#         sys.exit(1)
 
 if __name__ == "__main__":
     data = pd.read_csv('inventory.csv', encoding = "ISO-8859-1")   
     df = pd.DataFrame(data, columns= ['book','title','author','binding','pubdate','publisher','isbn10','isbn13','condition','dustjacket','signed','edition','price','descr','synopsis','about_auth'])
     
     
-    #book_clean(df)
-    title_clean(df)
+    book_clean(df)
+    #title_clean(df)
