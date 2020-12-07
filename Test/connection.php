1d8e2d12abeb3d67424a95ad83b44e08bdcb3016
ColeDavis99 <coledavis6499@gmail.com>
2020-12-07 15:39:54 -0600

    All Books Query displaying raw

--- Test/connection.php
+++ Test/connection.php
@@ -1,39 +1,48 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-<body>
-<?php
+  <body>
+    <?php
+      $hostname = "cs-class-db.srv.mst.edu";
+      $username = "cedtfh";
+      $password = "koobpasswordkoobpassword2!";
+      $db = "cedtfh";
 
-$hostname = "cs-class-db.srv.mst.edu";
-$username = "MSTusername";
-$password = "password";
-$db = "yourDB";
 
-$dbconnect=mysqli_connect($hostname,$username,$password,$db);
 
-if ($dbconnect->connect_error) {
-  die("Database connection failed: " . $dbconnect->connect_error);
-}
+      $dbconnect=mysqli_connect($hostname,$username,$password,$db);
 
-?>
+      if ($dbconnect->connect_error) {
+        die("Database connection failed: " . $dbconnect->connect_error);
+      }
+    ?>
 
-<table border="1" align="center">
-<tr>
-  <td>ALBUMS</td>
-</tr>
+    <table border="1" align="center">
+    <tr>
+      <td>Title</td>
+      <td>Author</td>
+      <td>Price</td>
+    </tr>
+ 
+    <?php
 
-<?php
+      $all_book_query ="SELECT Book.Title, Book.Price, AUTHOR.Name FROM BOOKS as Book
+                          NATURAL JOIN AUTHOR_BOOK
+                          JOIN AUTHOR on AUTHOR.AUTHOR_ID = AUTHOR_BOOK.Author_ID
+                          WHERE Book.Title IS NOT NULL
+                        ORDER BY Title";
 
-$query = mysqli_query($dbconnect, "SELECT * FROM ALBUMS")
-   or die (mysqli_error($dbconnect));
+      $query = mysqli_query($dbconnect, $all_book_query)
+        or die (mysqli_error($dbconnect));
 
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row[1]}</td>
-   </tr>\n";
- }
-
-?>
-</table>
-</body>
+      while ($row = mysqli_fetch_array($query)) {
+        echo
+        "<tr>
+          <td>{$row[0]}</td>
+          <td>{$row[2]}</td>
+          <td>{$row[1]}</td>
+        </tr>\n";
+      }
+    ?>
+  </table>
+  </body>
 </html>
