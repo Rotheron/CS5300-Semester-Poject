c33d912505cd213916055e445560d2ffaaf72766
ColeDavis99 <coledavis6499@gmail.com>
2020-12-10 17:48:09 -0600

    Basic pagination nav carousel working

--- Test/pagination.php
+++ Test/pagination.php
@@ -4,8 +4,11 @@
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="../CSS/pagination.css">
+    <link rel="stylesheet" href="../Libraries/Glider.js-master/glider.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="../Libraries/Glider.js-master/glider.js"></script>
+    <script src="../Scripts/my_script.js"></script>
 </head>
 <body>
     <?php
@@ -91,23 +94,36 @@
 
 
     <ul class="pagination">
-        <li><a href="?pageno=1">First</a></li>
+        <!-- <li><a href="?pageno=1">First</a></li>
         <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
             <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>">Prev</a>
-        </li>
-
-        <!-- Dynamically Generate the "in-between" pagination numbers -->
-        <?php
-            for ($i=2; $i<=$total_pages; $i++)
-            {
-                echo "<li><a href=\"?pageno=".$i."\">".$i."</a></li>";
-            }
-        ?>
-
+        </li> -->
+<!-- 
         <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
             <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
         </li>
-        <li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+        <li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li> -->
     </ul>
+
+
+
+    <div class="glider-contain">
+        <div class="glider">
+        <!-- Dynamically Generate the "in-between" pagination numbers -->
+            <?php
+                for ($i=1; $i<=$total_pages; $i++)
+                {
+                    echo "<div class=\"pagination_link_wrapper\">";
+                    echo "<a class=\"pagination_link\" href=\"?pageno=".$i."\">".$i."</a></div>\n";
+                }
+            ?> 
+        </div>
+        
+            <button aria-label="Previous" class="glider-prev">«</button>
+            <button aria-label="Next" class="glider-next">»</button>
+            <div role="tablist" class="dots"></div>
+    </div>
+
+
 </body>
 </html>
--- Test/search.php
+++ Test/search.php
@@ -4,8 +4,12 @@
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="../CSS/pagination.css">
+    <link rel="stylesheet" href="../Libraries/Glider.js-master/glider.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="../Libraries/Glider.js-master/glider.js"></script>
+    <script src="../Scripts/my_script.js"></script>
+
 </head>
 <body>
     <?php
@@ -91,7 +95,7 @@
                 JOIN BOOK_INFO on BOOK_INFO.Book_Info_ID = Book.Book_Info_ID
                 WHERE Book.Title IS NOT NULL
                 AND Book.Price IS NOT NULL
-                AND lower(AUTHOR.$search_param) like lower('%$search_value%')
+                AND lower(AUTHOR.$search_param) like lower('%$search_value%') 
                 ORDER BY Book.Title
                 LIMIT $offset, $no_of_records_per_page";
             }#if you are searching for isbn
@@ -157,24 +161,30 @@
     </div>
 
 
-    <ul class="pagination">
-        <li><a href="?pageno=1<?php echo "&search_value=".$search_value."&search_param=".$search_param;?>">First</a></li>
-        <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
-            <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1)."&search_value=".$search_value."&search_param=".$search_param; } ?>">Prev</a>
-        </li>
-
+    <div class="glider-contain">
+        <div class="glider">
         <!-- Dynamically Generate the "in-between" pagination numbers -->
-        <?php
-            for ($i=2; $i<=$total_pages; $i++)
-            {
-                echo "<li><a href=\"?pageno=".$i."&search_value=".$search_value."&search_param=".$search_param."\">".$i."</a></li>";
-            }
-        ?>
+            <?php
+                for ($i=1; $i<=$total_pages; $i++)
+                {
+                    echo "<div class=\"pagination_link_wrapper\">";
+                    echo "<a class=\"pagination_link\" href=\"?pageno=".$i."&search_value=".$search_value."&search_param=".$search_param."\">".$i."</a></div>\n";
+                }
+            ?> 
+        </div>
 
-        <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
-            <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1)."&search_value=".$search_value."&search_param=".$search_param; } ?>">Next</a>
+        <!-- <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
+            <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1)."&search_value=".$search_value."&search_param=".$search_param; } ?>">></a>
         </li>
-        <li><a href="?pageno=<?php echo $total_pages."&search_value=".$search_value."&search_param=".$search_param;?>">Last</a></li>
-    </ul>
+        <li><a href="?pageno=<?php echo $total_pages."&search_value=".$search_value."&search_param=".$search_param;?>">Last</a></li> -->
+
+
+
+        </div>
+
+        <button aria-label="Previous" class="glider-prev">«</button>
+        <button aria-label="Next" class="glider-next">»</button>
+        <div role="tablist" class="dots"></div>
+    </div>
 </body>
 </html>
