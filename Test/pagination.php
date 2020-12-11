2c024b234464e742f74878c6e089ccc2e25b58ca
Zachary Bingham <lilbing99@gmail.com>
2020-12-11 13:52:12 -0600

    starter modal functionality in place

--- Test/pagination.php
+++ Test/pagination.php
@@ -8,11 +8,33 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<script src="http://code.jquery.com/jquery.js"></script>
+<script type="text/javascript">
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close");
+var book = document.getElementsByClassName("book");
+ function bookClick(ID){
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    console.log()
+    }
+ function spanClick(){
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close");
+    modal.style.display = "none";
+}
+window.onclick = function(event){
+    var modal = document.getElementById("myModal");
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+}
+</script>
     <?php
       $hostname = "cs-class-db.srv.mst.edu";
-      $username = "lff8gw";
+      $username = "ztbmwf";
       $password = "pass";
-      $db = "lff8gw";
+      $db = "ztbmwf";
 
       $dbconnect=mysqli_connect($hostname,$username,$password,$db);
         if (isset($_GET['pageno'])) {
@@ -45,6 +67,13 @@
                     ORDER BY Book.Title
                     LIMIT $offset, $no_of_records_per_page ";
 
+        $book_info = "SELECT Book.Title, AUTHOR.Name, Book.Price, Book.Book_ID, BOOK_INFO.ISBN_10, BOOK_INFO.Pub_Date,Book FROM BOOKS as Book 
+                    NATURAL JOIN AUTHOR_BOOK
+                    JOIN AUTHOR on AUTHOR.AUTHOR_ID = AUTHOR_BOOK.Author_ID
+                    JOIN BOOK_INFO on BOOK_INFO.Book_Info_ID = Book.Book_Info_ID
+                    WHERE Book.Title IS NOT NULL
+                    AND Book.Price IS NOT NULL
+                    ORDER BY Book.Title ";
         if(!mysqli_query($dbconnect,$sql))
         {
             printf("Error: %s\n", mysqli_error($dbconnect));
@@ -75,7 +104,7 @@
                 #$imgJSON=\"https://www.googleapis.com/books/v1/volumes?q=isbn:\"{$row[4]}
                 #<img src={$imgJSON}> attempting some google books stuff but might not be needed
                 echo
-                "<div class=\"book\">
+                "<div class=\"book\" onclick=\"bookClick()\">
                     <img src=\"http://covers.openlibrary.org/b/isbn/{$row[4]}.jpg\" class=\"bookImg\">
                     <div class=\"bookText\">
                         <div><p>{$row[0]}</p></div>
@@ -88,7 +117,33 @@
             mysqli_close($dbconnect);
         ?>
     </div>
+    <div id="myModal" class="modal">
+    <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close" onclick="spanClick()">&times;</span>
+            <p>Some text in the Modal..</p>
+            <?php
+                #if the search button has been clicked, change the query to search
+                $res_data = mysqli_query($dbconnect,$sql);
+                //here goes the data
+                #$imgJSON=\"https://www.googleapis.com/books/v1/volumes?q=isbn:\"{$row[4]}
+                #<img src={$imgJSON}> attempting some google books stuff but might not be needed
+                echo
+                "<div class=\"book\" onclick=\"bookClick()\">
+                    <img src=\"http://covers.openlibrary.org/b/isbn/{$row[4]}.jpg\" class=\"bookImg\">
+                    <div class=\"bookText\">
+                        <div><p>{$row[0]}</p></div>
+                        <div><p>{$row[1]}</p></div>
+                        <div><p>{$row[2]}</p></div> 
+                        <div><p>{$row[3]}</p></div> 
+                    </div>
+                </div>";
+            
+            mysqli_close($dbconnect);
+            ?>
+        </div>
 
+    </div>
 
     <ul class="pagination">
         <li><a href="?pageno=1">First</a></li>
